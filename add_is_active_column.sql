725b58a9bbc2072ab3132f16c320efc309f7aa4e
Trần Anh Kiệt <kietccc21@gmail.com>
2025-10-31 12:42:35 +0700

    update clean

--- add_is_active_column.sql
+++
@@ -1,6 +0,0 @@
--- Thêm cột is_active vào bảng products
-ALTER TABLE products 
-ADD COLUMN is_active BOOLEAN DEFAULT TRUE NOT NULL;
-
--- Đặt tất cả sản phẩm hiện tại là đang kinh doanh
-UPDATE products SET is_active = TRUE WHERE is_active IS NULL;
